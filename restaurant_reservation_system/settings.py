2848b37486751dc5ca73a57f249fa51103e4c8c4
Hossein Mansouri <hossein.mansouri11@gmail.com>
2025-08-08 14:35:17 +0330

    feat: change database settings from sqlite to postgres

--- restaurant_reservation_system/settings.py
+++ restaurant_reservation_system/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "restaurant_reservation_system.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "reserve_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
